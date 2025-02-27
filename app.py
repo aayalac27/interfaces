4411657a7e4bfe1c9a5c5de404d8274a17a7f395
aayalac27 <aayalac@eafit.edu.co>
2025-02-27 10:47:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,33 @@ with col2:
     st.write("La vista es fundamental para tu interfaz")
   if modo == "Auditivo":
     st.write("La audición es fundamental para tu interfaz")
+if modo == "Táctil":
+    st.write("El tacto es esencial para tu interfaz")
+
+
+st.subheader("Uso de Botones")
+if st.button('Presiona el botón'):
+    st.write('Gracias por presionar')
+else:
+    st.write('No has presionado aún')
+
+st.subheader("Selectbox")
+in_mod = st.selectbox(
+  "Selecciona la modalidad",
+  ("Audio", "Visual", "Háptico"),
+)
+if in_mod == "Audio":
+    set_mod = "Reproducir audio"
+elif in_mod == "Visual":
+    set_mod = "Reproducir video"
+elif in_mod == "Háptico":
+    set_mod = "Activar vibración"
+st.write(" La acción es:", set_mod)
+
+
+with st.sidebar:
+  st.subheader("Configura la modalidad")
+  mod_radio = st.radio(
+    "Escoge la modalidad a usar",
+    ("Visual", "Auditiva", "Háptica")
+  )
