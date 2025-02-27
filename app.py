9fdb4f3bb4c331fee476bc831dbd2d68c14ea8e4
aayalac27 <aayalac@eafit.edu.co>
2025-02-27 10:14:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.title("mi primera app!")
 st.header("en este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
 st.write("faclmente puedo realizar backend y frontend")
 image = Image.open('image.jpeg')
+
+st.image(image, caption="ggg")
