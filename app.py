e0bcbc9578287770ba01f1255f242946b74fc03b
aayalac27 <aayalac@eafit.edu.co>
2025-02-27 10:21:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ st.write("faclmente puedo realizar backend y frontend")
 image = Image.open('image.jpeg')
 
 st.image(image, caption="Polos opuestos")
+
+
+texto = st.text_imput('sisas la k','la cara del trap de medallo')
+st.write('el texto escrito es', texto
